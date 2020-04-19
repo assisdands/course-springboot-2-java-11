de93f6903bd314483b4efbde760891f79d4d111b
Dandara de Assis <dandara.assis@al.resilia.com.br>
2020-04-19 18:33:52 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users" )
+public class UserResource {
+	
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com","99999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
